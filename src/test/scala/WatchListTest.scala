fdfa3cf82962d77fffb7723a0739da8121b99e4f
philhannant <philip.a.hannant@gmail.com>
2018-06-23 15:36:11 +0100

    deleteContentID test added

--- src/test/scala/WatchListTest.scala
+++ src/test/scala/WatchListTest.scala
@@ -1,3 +1,4 @@
+
 import org.scalatest._
 
 class WatchListTest extends FlatSpec with Matchers{
@@ -12,6 +13,16 @@ class WatchListTest extends FlatSpec with Matchers{
     val expected = List("zRE49", "wYqiZ", "15nW5")
     watchList.getContentIDs() should contain theSameElementsAs expected
 
+  }
+
+  "a WatchLst" should "be able to delete a contentID from the list" in {
+    watchList.addContentIDs("zRE49")
+    watchList.addContentIDs("wYqiZ")
+    watchList.addContentIDs("15nW5")
+    watchList.deleteContentID("fefrefe")
+    watchList.deleteContentID("wYqiZ")
+    val expected = List("zRE49", "15nW5")
+    watchList.getContentIDs() should contain theSameElementsAs expected
   }
 
 }
