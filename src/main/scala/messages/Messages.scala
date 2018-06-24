3fd68f80adb49f7edea5d90fbd73071752602b9e
philhannant <philip.a.hannant@gmail.com>
2018-06-24 09:04:26 +0100

    CustomerWatchList message added

--- src/main/scala/messages/Messages.scala
+++ src/main/scala/messages/Messages.scala
@@ -1,11 +1,11 @@
 package messages
 
 sealed trait Messages
-
-    case class ActionPerformed(description: String)
-    case class GetWatchList(customer: String)
-    case class AddContentID(customer: String, contentID: String)
-    case class GetUser(name: String)
-    case class DeleteContentID(customer: String, contentID: String)
+case class ActionPerformed(description: String)
+case class GetWatchList(customer: String)
+case class AddContentID(customer: String, contentID: String)
+case class GetUser(name: String)
+case class DeleteContentID(customer: String, contentID: String)
+case class CustomerWatchList(contentIDs: List[String])
 
 
