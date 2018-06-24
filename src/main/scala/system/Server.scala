51110c3b8aee3ba6dabeeed78cdd9a431019e703
philhannant <philip.a.hannant@gmail.com>
2018-06-24 11:40:43 +0100

    Forgot to commit server

---
+++ src/main/scala/system/Server.scala
@@ -0,0 +1,32 @@
+package system
+
+//#quick-start-server
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
+
+import actors.CustomerRegisterActor
+
+import akka.actor.{ ActorRef, ActorSystem }
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+
+//#main-class
+object Server extends App with CustomerRoutes {
+
+
+  implicit val system: ActorSystem = ActorSystem("akkaHttpServer")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+  val customerRegisterActor: ActorRef = system.actorOf(CustomerRegisterActor.props, "customerRegisterActor")
+
+  lazy val routes: Route = customerRoutes
+
+  Http().bindAndHandle(routes, "localhost", 8080)
+
+  println(s"Server online at http://localhost:8080/")
+
+  Await.result(system.whenTerminated, Duration.Inf)
+
+}
+
